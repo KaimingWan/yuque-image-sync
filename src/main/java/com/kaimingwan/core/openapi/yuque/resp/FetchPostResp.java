6dd38b1bc956ccb4e57532e341eafa24833da916
KamiWan <344277934@qq.com>
2024-01-30 17:14:18 +0800

    fix: compatible with new API

--- src/main/java/com/kaimingwan/core/openapi/yuque/resp/FetchPostResp.java
+++ src/main/java/com/kaimingwan/core/openapi/yuque/resp/FetchPostResp.java
@@ -1,5 +1,6 @@
 package com.kaimingwan.core.openapi.yuque.resp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.kaimingwan.core.openapi.yuque.model.YuqueDoc;
 import java.util.List;
 import lombok.Data;
@@ -8,6 +9,7 @@ import lombok.Data;
  * @author wanshao create time is  2022/10/31
  **/
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FetchPostResp {
 
   private List<YuqueDoc> data;
